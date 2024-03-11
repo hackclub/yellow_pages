c27cdacdafea7eb1f3aad76b9ff16e99e2226599
Max Wofford <max@maxwofford.com>
2024-03-10 22:51:10 -0700

    Add tests for lookups

--- spec/yellow_pages/merchant_spec.rb
+++ spec/yellow_pages/merchant_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe YellowPages::Merchant do
   it "has a lookup method" do
     expect(YellowPages::Merchant).to respond_to(:lookup)
   end
+
+  it "returns a merchant name for a found network ID" do
+    expect(YellowPages::Merchant.lookup(network_id: "1234567890")).to eq("Rocket Rides")
+  end
+
+  it "returns nil for an unfound network ID" do
+    expect(YellowPages::Merchant.lookup(network_id: "NONEXISTANT_MERCHANT_ID")).to be_nil
+  end
 end
